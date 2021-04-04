6612841b66c3d9faf76a3ec31aec1c48c88bb7f6
aliaksei6777 <aliaksei6777@gmail.com>
2021-04-04 14:27:50 +0300

    init commit

---
+++ src/containers/CurrencyExchangeContainer/CurrencyEContainer.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import CurrencyExchange from '../../components/CurrencyExchange/CurrencyExchange';
+import { CurrencyType } from '../../redux/currencyReducer';
+import { Dispatch } from 'redux';
+import {
+    ChangeActionAC,
+    ChangeCurrencyFieldAC,
+    СhangeCurrentCurrencyAC,
+    CurrencyReducersTypes
+} from '../../redux/actions';
+import {useDispatch, useSelector} from 'react-redux';
+import {
+    selectAmountOfBYN,
+    selectAmountOfCurrency,
+    selectCurrencies,
+    selectCurrentCurrency,
+    selectIsBuying
+} from "../../redux/selector";
+
+const CurrencyEContainer: React.FC = () => {
+
+    const dispatch = useDispatch<Dispatch<CurrencyReducersTypes>>()
+    const currencies = useSelector(selectCurrencies)
+    const currentCurrency = useSelector(selectCurrentCurrency)
+    const isBuying = useSelector(selectIsBuying)
+    const amountOfBYN = useSelector(selectAmountOfBYN)
+    const amountOfCurrency = useSelector(selectAmountOfCurrency)
+
+    let currencyRate: number = 0;
+    const currenciesName = currencies.map((currency: CurrencyType) => {
+        if (currency.currencyName === currentCurrency) {
+            currencyRate = isBuying ? currency.buyRate : currency.sellRate;
+        }
+        return currency.currencyName;
+    });
+
+    const changeCurrencyField = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let value = e.currentTarget.value;
+        if (!isFinite(+value)) return;
+        if (e.currentTarget.dataset.currency) {
+            const trigger: string = e.currentTarget.dataset.currency;
+            if (trigger === 'byn') {
+                if (value === '') {
+                    dispatch(ChangeCurrencyFieldAC(value, value));
+                } else {
+                    dispatch(ChangeCurrencyFieldAC(value, (+Number(value).toFixed(2) / currencyRate).toFixed(2)));
+                }
+            } else {
+                if (value === '') {
+                    dispatch(ChangeCurrencyFieldAC(value, value));
+                } else {
+                    dispatch(ChangeCurrencyFieldAC((+Number(value).toFixed(2) * currencyRate).toFixed(2), value));
+                }
+            }
+        }
+    };
+    const changeAction = (e: React.MouseEvent<HTMLSpanElement>) => {
+        e.currentTarget.dataset.action === 'buy' ? dispatch(ChangeActionAC(true)) : dispatch(ChangeActionAC(false));
+    };
+
+    const changeCurrentCurrency = (e: React.MouseEvent<HTMLLIElement>) => {
+        e.currentTarget.dataset.currency && dispatch(СhangeCurrentCurrencyAC(e.currentTarget.dataset.currency));
+    };
+
+    return (
+        <React.Fragment>
+            <CurrencyExchange
+                currenciesName={currenciesName}
+                currentCurrency={currentCurrency}
+                currencyRate={currencyRate}
+                isBuying={isBuying}
+                amountOfBYN={amountOfBYN}
+                amountOfCurrency={amountOfCurrency}
+                changeCurrencyField={changeCurrencyField}
+                changeAction={changeAction}
+                changeCurrentCurrency={changeCurrentCurrency}
+            />
+        </React.Fragment>
+    );
+};
+
+export default CurrencyEContainer
